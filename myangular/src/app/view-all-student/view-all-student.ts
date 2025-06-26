b9bbff15ab6d9659bae8b3df1f850796a5a803fc
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-06-26 15:25:24 +0600

    add
    add

--- myangular/src/app/view-all-student/view-all-student.ts
+++ myangular/src/app/view-all-student/view-all-student.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { StudentService } from '../service/student.service';
 import { Router } from '@angular/router';
+import { Updatestudent } from '../updatestudent/updatestudent';
 
 
 @Component({
@@ -24,31 +25,24 @@ export class ViewAllStudent implements OnInit {
 
 
   loadAllStudent() {
-
     this.students = this.studentService.getAllStudent();
-
   }
+
+
   deleteStudent(id: string): void {
     this.studentService.deleteStudent(id).subscribe({
-
       next: (res) => {
-
         console.log("Student Delete");
         this.cdr.reattach();
         this.loadAllStudent();
-
       },
-
       error: (err) => {
         console.log(err);
       }
-    })
-
-
-
-
+    });
   }
-  
 
-
+  updateStudent(id: string): void {
+    this.router.navigate(['updatestudent', id]);
+  }
 }
