4fff19d754b60b55331080a4c4a362320e85cca9
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-06-29 18:47:31 +0600

    add

---
+++ courier/src/app/app-routing-module.ts
@@ -0,0 +1,14 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { Home } from './home/home';
+
+const routes: Routes = [
+{  path:'',component:Home}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
