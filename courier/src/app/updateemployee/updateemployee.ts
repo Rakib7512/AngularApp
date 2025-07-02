73937d08114b9383e2dc3a205e580652e8e3007e
Md Rakib Islam <mdrakibislam7512@gmail.com>
2025-07-03 01:05:41 +0600

    add
    add

--- courier/src/app/updateemployee/updateemployee.ts
+++ courier/src/app/updateemployee/updateemployee.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Employee } from '../../model/employee.model';
+import { EmployeeService } from '../service/employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-updateemployee',
@@ -6,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './updateemployee.html',
   styleUrl: './updateemployee.css'
 })
-export class Updateemployee {
+export class Updateemployee implements OnInit {
+  id: string = '';
+  employee: Employee = new Employee();
 
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+    this.loadStudentById();
+  }
+
+  loadStudentById() {
+    // nicher id ta holo service class ar update ar return ar id
+    this.id = this.route.snapshot.params['id'];
+    this.employeeService.getEmplpyeeById(this.id).subscribe({
+      next: (res) => {
+        this.employee = res;
+        this.cdr.markForCheck();
+      },
+      error: (err) => {
+        console.error('error fetching student:', err)
+      }
+    });
+  }
+
+  updateStudent(): void {
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe({
+      next: () => this.router.navigate(['/allStudent']),
+      error: (err) => console.error('update fail', err)
+    });
+  }
 }
